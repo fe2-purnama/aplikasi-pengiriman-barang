0bc2d6faeb987afe9f9055a574be664216371165
NarrIzzy <dnrfian168@gmail.com>
2024-06-12 16:13:53 +0700

    add-readme.md

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -26,26 +26,26 @@ module.exports = async function (req, res, next) {
 
     const payload = jwt.verify(token, JWT_SECRET_KEY);
 
-    const user = await User.findById(payload.id);
-    if (!user) {
-      return res.status(401).json({
-        status: false,
-        message: "Tidak terautentikasi",
-        err: "Email tidak terdaftar",
-        data: null,
-      });
-    }
+    // const user = await User.findById(payload.id);
+    // if (!user) {
+    //   return res.status(401).json({
+    //     status: false,
+    //     message: "Tidak terautentikasi",
+    //     err: "Email tidak terdaftar",
+    //     data: null,
+    //   });
+    // }
 
-    req.user = user;
+    // req.user = user;
 
-    if (!req.user.isVerified) {
-      return res.status(401).json({
-        status: false,
-        message: "Tidak diizinkan",
-        err: "Anda perlu memverifikasi email Anda untuk melanjutkan",
-        data: null,
-      });
-    }
+    // if (!req.user.isVerified) {
+    //   return res.status(401).json({
+    //     status: false,
+    //     message: "Tidak diizinkan",
+    //     err: "Anda perlu memverifikasi email Anda untuk melanjutkan",
+    //     data: null,
+    //   });
+    // }
 
     next();
   } catch (err) {
---
+++ readme.md
@@ -0,0 +1,16 @@
+# App Pengiriman Barang
+
+BaseUrl = https://kirimkan-be.vercel.app
+
+
+
+## Entity Relationship Diagram
+
+
+
+</br></br>
+<img width="600" alt="image" src="https://raw.githubusercontent.com/Danarzlf/BE-AppPengirimanBarang/master/public/images/ERD-COY.png">
+
+
+
+

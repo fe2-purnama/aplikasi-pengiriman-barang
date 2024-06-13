51826e44459c877dca9f2a0e3b985030fba93f64
Nadya Putri Amalia <putrinadya754@gmail.com>
2024-06-13 10:02:27 +0700

    footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,15 +1,35 @@
 <template>
-    <footer>
-        <h1>Footer</h1>
-    </footer>
+  <footer>
+    <div class="container">
+      <div class="row gy-4">
+        <div
+          class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center"
+          data-aos="zoom-out"
+        >
+          <h1>Welcome to <span>BizLand</span></h1>
+          <p>
+            We are team of talented designers making websites with Bootstrap
+          </p>
+          <div class="d-flex">
+            <a href="#about" class="btn-get-started">Get Started</a>
+            <a
+              href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+              class="glightbox btn-watch-video d-flex align-items-center"
+              ><i class="bi bi-play-circle"></i><span>Watch Video</span></a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style>
-    footer {
-        position: fixed;
-        bottom: 0;
-    }
-    h1 {
-        text-align: center;
-    }
+footer {
+  position: fixed;
+  bottom: 0;
+}
+h1 {
+  text-align: center;
+}
 </style>

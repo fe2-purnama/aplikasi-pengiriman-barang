3e99695f8e18a9db84e81e4950d77052b39717b9
Nadya Putri Amalia <putrinadya754@gmail.com>
2024-06-13 10:13:15 +0700

    footer lumayan

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,35 +1,28 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="row gy-4">
-        <div
-          class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center"
-          data-aos="zoom-out"
-        >
-          <h1>Welcome to <span>BizLand</span></h1>
-          <p>
-            We are team of talented designers making websites with Bootstrap
-          </p>
-          <div class="d-flex">
-            <a href="#about" class="btn-get-started">Get Started</a>
-            <a
-              href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-              class="glightbox btn-watch-video d-flex align-items-center"
-              ><i class="bi bi-play-circle"></i><span>Watch Video</span></a
-            >
-          </div>
-        </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          &copy; 2024 Your Company. All rights reserved.
+        </p>
       </div>
-    </div>
-  </footer>
-</template>
-
-<style>
-footer {
-  position: fixed;
-  bottom: 0;
-}
-h1 {
-  text-align: center;
-}
-</style>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  }
+  </script>
+  
+  <style scoped>
+  .footer {
+    background-color: #f8f9fa;
+    padding: 1rem 0;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    box-shadow: 0 -1px 5px rgba(0,0,0,0.1);
+  }
+  </style>
+  
